b8e5db570a457a963258fbaefb1cdde1b1cb8d6b
Vladimir <vovan2.01@bk.ru>
2023-09-25 02:39:15 +1000

    Update Program.cs

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -18,22 +18,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-//Заполнение БД START
-else
-{
-    app.UseDeveloperExceptionPage();
-    app.UseMigrationsEndPoint();
-}
-
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-
-    var context = services.GetRequiredService<SchoolContext>();
-    context.Database.EnsureCreated();
-    DbInitializer.Initialize(context);
-}
-//Заполнение БД END
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
